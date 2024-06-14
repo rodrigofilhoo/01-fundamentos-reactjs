ef5f3a615adcc487be350358878f4e4abe6e170a
Rodrigo Filho <rodrigofilhogt@gmail.com>
2024-06-13 22:15:40 -0300

    Add Post Content

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -18,18 +18,22 @@ export function Post() {
                     dateTime="2024-06-12 15:03:00">
                     Publicado a 1h
                 </time>
-
-                <div className={styles.content}>
-                    <p>Fala galeraa 👋</p>
-                    <p>
-                        Acabei de subir mais um projeto no meu portifa.
-                        É um projeto que fiz no NLW Return, evento da Rocketseat.
-                        O nome do projeto é DoctorCare 🚀
-                    </p>
-                    <a href="#"><p>👉 jane.design/doctorcare</p></a>
-                    <a href="#"><p>#novoprojeto #nlw #rocketseat</p></a>
-                </div>
             </header>
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>
+                    Acabei de subir mais um projeto no meu portifa.
+                    É um projeto que fiz no NLW Return, evento da Rocketseat.
+                    O nome do projeto é DoctorCare 🚀
+                </p>
+                <p>👉<a href="#"> jane.design/doctorcare</a></p>
+                <p>
+                    <a href="#">#novoprojeto </a>
+                    <a href="#">#nlw </a>
+                    <a href="#">#rocketseat </a>
+                </p>
+            </div>
+
         </article>
     )
 }
